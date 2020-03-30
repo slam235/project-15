129b527146c6afdd2aca96c277f8030154af3d38
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-03-30 18:12:35 +0300

    Написал возможные коды ответов

--- controllers/cards.js
+++ controllers/cards.js
@@ -5,27 +5,61 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: ` Произошла ошибка при создании карточки - ${err}` }));
+    .then((card) => {
+      if (!name || !link) {
+        res.status(400).send({ message: 'Имя и ссылка должы быть заполнены!' });
+      } else {
+        res.status(200).send({ data: card });
+      }
+    })
+    .catch((err) => res.status(500).send({ message: `Internal Server Error - ${err}` }));
 };
+
 module.exports.getCards = (req, res) => {
   Card.find({})
     .populate('owner')
-    .then((cards) => res.send({ data: cards }))
+    .then((cards) => res.status(200).send({ data: cards }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при поиске карточек' }));
 };
+
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then((card) => res.status(404).send(card !== null ? { data: card } : { data: 'Карточка не найдена' }))
+    .then((card) => {
+      if (!req.params.cardId) {
+        res.status(400).send({ message: 'Некорректный Id' });
+      } else if (!card) {
+        res.status(404).send({ message: 'Не найдена какрточка с таким Id' });
+      } else {
+        res.status(200).send({ data: card });
+      }
+    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при удалении карточки' }));
 };
+
 module.exports.likeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(404).send(card !== null ? { data: card } : { data: 'Карточка не найдена' }))
+    .then((card) => {
+      if (!req.params.cardId) {
+        res.status(400).send({ message: 'Некорректный Id' });
+      } else if (!card) {
+        res.status(404).send({ message: 'Не найдена какрточка с таким Id' });
+      } else {
+        res.status(200).send({ data: card });
+      }
+    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при лайке карточки' }));
 };
+
 module.exports.dislikeCard = (req, res) => {
   Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
-    .then((card) => res.status(404).send(card !== null ? { data: card } : { data: 'Карточка не найдена' }))
+    .then((card) => {
+      if (!req.params.cardId) {
+        res.status(400).send({ message: 'Некорректный Id' });
+      } else if (!card) {
+        res.status(404).send({ message: 'Не найдена какрточка с таким Id' });
+      } else {
+        res.status(200).send({ data: card });
+      }
+    })
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при дизлайке карточки' }));
 };
--- controllers/users.js
+++ controllers/users.js
@@ -4,29 +4,57 @@ module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка при создании пользователя - ${err}` }));
+    .then((user) => {
+      if (!avatar || !name || !about) {
+        res.status(400).send({ message: 'Имя, описание и ссылка на аватар должны быть заполнены!' });
+      } else {
+        res.status(200).send({ data: user });
+      }
+    })
+    .catch((err) => res.status(500).send({ message: `Internal Server Error - ${err}` }));
 };
+
 module.exports.getUsers = (req, res) => {
   User.find({})
-    .then((users) => res.send({ data: users }))
+    .then((users) => res.status(200).send({ data: users }))
     .catch(() => res.status(500).send({ message: 'Произошла ошибка при поиске пользователей' }));
 };
+
 module.exports.getSingleUser = (req, res) => {
   User.findById(req.params.id)
-    .then((user) => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Нет пользователя с таким Id' }));
+    .then((user) => {
+      if (!req.params.id) {
+        res.status(400).send({ message: 'Некорректный Id пользователя' });
+      } else {
+        res.status(200).send({ data: user });
+      }
+    })
+    .catch(() => res.status(404).send({ message: 'Нет пользователя с таким Id' }));
 };
+
 module.exports.updateUser = (req, res) => {
   const { name, about } = req.body;
   User.findByIdAndUpdate(req.user._id, { name, about },
     { new: true, runValidators: true, upsert: true })
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!req.user._id) {
+        res.status(403).send({ message: 'Не найден пользователь с таким Id' });
+      } else {
+        res.status(200).send({ data: user });
+      }
+    })
     .catch((err) => res.status(500).send({ message: `Произошла ошибка при обновлении профиля - ${err}` }));
 };
+
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
   User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true, upsert: true })
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!req.user._id) {
+        res.status(403).send({ message: 'Не найден пользователь с таким Id' });
+      } else {
+        res.status(200).send({ data: user });
+      }
+    })
     .catch((err) => res.status(500).send({ message: `Произвошла ошибка при обновлении аватарки - ${err}` }));
 };
