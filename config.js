28ff34b00ce8b55023e33d20215899930e4f2f45
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-04-23 02:03:29 +0300

    Добавил переменную среду, для выбора секретного ключа

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
+const dev = process.env.NODE_ENV === 'development';
 module.exports = {
-  JWT_SECRET: 'secret key',
+  JWT_SECRET: dev ? 'secret key' : process.env.JWT_SECRET,
   PORT: 3000 || process.env.PORT,
   MONGO_URI: 'mongodb://localhost:27017/mestodb',
 };
