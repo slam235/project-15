ebc8b3740b5edee0ef296bbeb8f3c2800ef0e027
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-04-23 01:58:57 +0300

    Добавил валидацию на роутере

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,33 @@
 const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.post('/', createCard);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(8),
+    link: Joi.string().required().pattern(/^https?:\/\/(w{3}\.)?[\w-/.]{1,}/),
+  }),
+}), createCard);
+
 router.get('/', getCards);
-router.delete('/:cardId', deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), deleteCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), likeCard);
+
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().length(24),
+  }),
+}), dislikeCard);
 module.exports = router;
