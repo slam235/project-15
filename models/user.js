27353e97e139e569452a218850e01e2f4c8b76cc
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-04-23 01:55:11 +0300

    Исправил обработку ошибки через экземпляр класса

--- models/user.js
+++ models/user.js
@@ -2,6 +2,8 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
+const UnathorizedError = require('../errors/unauthorized-err');
+
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -51,12 +53,12 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new UnathorizedError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new UnathorizedError('Неправильные почта или пароль'));
           }
           return user;
         });
