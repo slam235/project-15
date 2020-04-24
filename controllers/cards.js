c36cbab98e32048749c68b06d396c561fe038854
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-04-25 00:22:25 +0300

    Добавил orFail(), equals(), использовал единый catch() в блоке вложенных промисов

--- controllers/cards.js
+++ controllers/cards.js
@@ -18,30 +18,27 @@ module.exports.getCards = (req, res, next) => {
 };
 
 module.exports.deleteCard = (req, res, next) => {
-  Card.findById(req.params.cardId)
+  Card.findById(req.params.cardId).orFail(new NotFoundError('Карточка не найдена'))
     .then((card) => {
-      if (!card) throw new NotFoundError('Карточка не найдена');
-      if ((card.owner).toString() !== req.user._id) throw new ForbiddenError('Карта не ваша! Удалить нельзя!');
+      if ((card.owner).equals(req.user._id) !== true) throw new ForbiddenError('Карта не ваша! Удалить нельзя!');
       Card.remove(card)
-        .then((cardToDelete) => res.send(cardToDelete !== null ? { data: card } : { data: 'Нечего удалять' }))
-        .catch(next);
+        .then((cardToDelete) => res.send(cardToDelete !== null ? { data: card } : { data: 'Нечего удалять' }));
+      return '';
     })
     .catch(next);
 };
 
 module.exports.likeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true })
+  Card.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: req.user._id } }, { new: true }).orFail(new NotFoundError('Карточка не найдена'))
     .then((card) => {
-      if (!card) throw new NotFoundError('Карточка не найдена');
       res.status(200).send({ data: card });
     })
     .catch(next);
 };
 
 module.exports.dislikeCard = (req, res, next) => {
-  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true })
+  Card.findByIdAndUpdate(req.params.cardId, { $pull: { likes: req.user._id } }, { new: true }).orFail(new NotFoundError('Карточка не найдена'))
     .then((card) => {
-      if (!card) throw new NotFoundError('Карточка не найдена');
       res.status(200).send({ data: card });
     })
     .catch(next);
