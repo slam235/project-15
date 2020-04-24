504e2d7f16c7e1dab9033021e722562fb4c6a2a1
Alexey Shiryaev <ashiryaev67@yandex.ru>
2020-04-25 00:34:40 +0300

    Сделел режим разработки по умолчанию

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
-const dev = process.env.NODE_ENV === 'development';
+require('dotenv').config();
+
+const prod = process.env.NODE_ENV === 'production';
 module.exports = {
-  JWT_SECRET: dev ? 'secret key' : process.env.JWT_SECRET,
+  JWT_SECRET: prod ? process.env.JWT_SECRET : 'secret key',
   PORT: 3000 || process.env.PORT,
   MONGO_URI: 'mongodb://localhost:27017/mestodb',
 };
